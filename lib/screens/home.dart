a7bbfe3ac05bc6c29b70ffc1e96daf4c56fee578
Pawel-Zmarlak <zmarlakpawel@gmail.com>
2021-02-22 20:47:18 +0100

    Screens updated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:registration_app/screens/login.dart';
 
 class HomeScreen extends StatelessWidget {
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          child: Text("Log Out"),
+          onPressed: () {
+            auth.signOut();
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => LoginScreen()));
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:registration_app/screens/home.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -25,7 +26,7 @@ class _LoginScreenState extends State<LoginScreen> {
               decoration: InputDecoration(hintText: 'Email'),
               onChanged: (value) {
                 setState(() {
-                  _email = value.trim(); //to remove all whitespaces
+                  _email = value.trim();
                 });
               },
             ),
@@ -35,36 +36,40 @@ class _LoginScreenState extends State<LoginScreen> {
             child: TextField(
               obscureText: true,
               decoration: InputDecoration(hintText: 'Password'),
-              // to tie text fields to variables created at the beginning
               onChanged: (value) {
                 setState(() {
-                  _password = value.trim(); //to remove all whitespaces
+                  _password = value.trim();
                 });
               },
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              RaisedButton(
-                color: Colors.red,
-                // color: Theme.of(context).accentColor,
-                child: Text('Sign In'),
-                onPressed: () {
-                  auth.signInWithEmailAndPassword(
-                      email: _email, password: _password);
-                },
-              ),
-              RaisedButton(
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            RaisedButton(
                 color: Theme.of(context).accentColor,
-                child: Text('Sign Up'),
+                child: Text('Signin'),
                 onPressed: () {
-                  auth.signInWithEmailAndPassword(
-                      email: _email, password: _password);
-                },
-              )
-            ],
-          )
+                  auth
+                      .signInWithEmailAndPassword(
+                          email: _email, password: _password)
+                      .then((_) {
+                    Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  });
+                }),
+            RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('Signup'),
+              onPressed: () {
+                auth
+                    .createUserWithEmailAndPassword(
+                        email: _email, password: _password)
+                    .then((_) {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                });
+              },
+            )
+          ])
         ],
       ),
     );
